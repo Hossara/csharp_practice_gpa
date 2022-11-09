9443c5fdade4898f70d1468b89a9745e0270e100
Hossara <hoseinaraghi84@gmail.com>
2022-11-09 12:52:40 +0330

    done

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace csharp_practice_gpa
 {
@@ -8,6 +6,23 @@ namespace csharp_practice_gpa
     {
         static void Main(string[] args)
         {
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine("=== Student {0} ===", i + 1);
+
+                int sum = 0;
+
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(">> Lession {0} : ", j + 1);
+                    sum += int.Parse(Console.ReadLine());
+                }
+
+                Console.WriteLine("----------");
+                Console.WriteLine("GPA : {0}", sum / 10);
+            }
+
+            Console.ReadKey();
         }
     }
 }
