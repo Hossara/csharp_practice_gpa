615e2a363b0e61f56ef2372febed525c62d3bad8
Hossara <hoseinaraghi84@gmail.com>
2022-11-09 08:55:09 +0330

    Init

---
+++ csharp_practice_gpa/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharp_practice_gpa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
